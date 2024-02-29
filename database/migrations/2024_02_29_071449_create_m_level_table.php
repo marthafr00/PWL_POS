c3784661c4a7be7c1b5bf3d7e3ae0279ed0462f9
marthafr00 <afrhissamartha771@gmail.com>
2024-02-29 14:29:30 +0700

    pertemuan 3_praktikum migration 1

--- database/migrations/2024_02_29_071449_create_m_level_table.php
+++ database/migrations/2024_02_29_071449_create_m_level_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_level', function (Blueprint $table) {
-            $table->id('level_id'); // membuat kolom PK dengan nama level_id yang auto increment
-            $table->string('level_kode', 10)->unique(); // membuat kolom level_kode dengan tipe data varchar(10) dan unique
-            $table->string('level_nama', 100); // membuat kolom created_at dan updated_at
+            $table->id('level_id');
+            $table->string('level_kode', 10)->unique();
+            $table->string('level_nama', 100);
             $table->timestamps();
         });
     }
