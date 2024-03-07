5d77c99ab33869e07632eb83c68bd0e8513ce925
marthafr00 <afrhissamartha771@gmail.com>
2024-03-07 13:12:03 +0700

    pertemuan 4_praktikum 1 update

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,12 +8,17 @@
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
-                <th>ID Level</th>
                 <th>Username</th>
                 <th>Nama</th>
-                <th>Password</th>
+                <th>ID Level Pengguna</th>
             </tr>
-            @foreach ($data as $d)
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->user_id }}</td>
+            </tr>
+        <!--    @foreach ($data as $d)
             <tr>
                 <td>{{ $d->user_id }}</td>
                 <td>{{ $d->level_id }}</td>
@@ -21,6 +26,6 @@
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->password }}</td>
             </tr>
-            @endforeach
+            @endforeach -->
         </table>
     </body>
