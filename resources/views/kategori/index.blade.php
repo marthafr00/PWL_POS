5c6839f07d1f3cf94f2880b5352f0f4f6655b7f2
marthafr00 <afrhissamartha771@gmail.com>
2024-03-18 14:04:59 +0700

    pertemuan 5_praktikum 2

---
+++ resources/views/kategori/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+{{--Customize layout sections--}}
+
+@section('subtitle', 'Kategori')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Kategori')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">Manage Kategori
+                <a href="../public/kategori/create" class="btn btn-primary float-right">Add</a>
+            </div>
+            <div class="card-body">
+                {{ $dataTable->table() }}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('js')
+    {{ $dataTable->scripts() }}
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     @hasSection('subtitle') | @yield('subtitle') @endif
 @stop
 
+@vite('resources/js/app.js')
+
 {{-- Extend and customize the page content header --}}
 
 @section('content_header')
@@ -47,18 +49,13 @@
 {{-- Add common Javascript/Jquery code --}}
             
 @push('js')
-<script>
-            
-    $(document).ready(function() {
-         // Add your common script logic here...
-    });
-            
-</script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
 @endpush
 
 {{-- Add common CSS customizations --}}
 
 @push('css')
+<link href="https://cdn/datatables.net/2.0.2/css/dataTables.dataTables.css"/>
 <style type="text/css">
             
     {{-- You can add AdminLTE customizations here --}}
