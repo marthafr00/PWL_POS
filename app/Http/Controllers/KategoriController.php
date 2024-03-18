653e2ab7995776a0a234861f3a2888fad4f895ef
marthafr00 <afrhissamartha771@gmail.com>
2024-03-18 14:08:45 +0700

    pertemuan 5_praktikum 3

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -5,16 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\KategoriModel;
+use App\Datatables\KategoriDataTable;
 
 class KategoriController extends Controller
 {
-    public function index(){
-             $data = [
+    public function index(KategoriDataTable $dataTable){
+        return $dataTable->render('kategori.index');
+    }
+
+    public function create(){
+        return view('kategori.create');
+    }
+
+    public function store(Request $request){
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
+
+
+    
+
+        /*     $data = [
             'kategori_kode' => 'SNK',
             'kategori_nama' => 'Snack/Makanan Ringan',
             'created_at' => now()
         ];
-        KategoriModel::insert($data);
+        KategoriModel::insert($data); */
 
         // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
         // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row. ' baris';
@@ -24,7 +43,6 @@ class KategoriController extends Controller
 
         // $data = DB::table('m_kategori')->get();
         // return view('kategori', ['data' => $data]);
-        $kategori = KategoriModel::all();
-        return view('kategori', ['data' => $kategori]);
-    }
+        // $kategori = KategoriModel::all();
+        // return view('kategori', ['data' => $kategori]);
 }
