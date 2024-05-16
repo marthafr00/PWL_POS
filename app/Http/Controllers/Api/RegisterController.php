3f2ac099607ff042858eb6abbe5c44585271de40
marthafr00 <afrhissamartha771@gmail.com>
2024-05-16 15:40:59 +0700

    pertemuan 13_praktikum 3

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -6,7 +6,6 @@ use App\Models\UserModel;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use PDO;
 
 class RegisterController extends Controller
 {
